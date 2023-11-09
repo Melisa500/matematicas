7eaf1331d060958b5334dc5a581d14033f1e7b9a
Melisa500 <melisabarrera24@gmail.com>
2023-11-09 00:13:23 -0300

    se cargaron todos las formulas

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ const perimetroDelCuadrado = document.querySelector('.perimetroDelCuadrado');
 const areaDelCuadrado = document.querySelector('.areaDelCuadrado');
 const perimetroDelTriangulo = document.querySelector('.perimetroDelTriangulo');
 const areaDelTriangulo = document.querySelector('.areaDelTriangulo');
+const circunsferencia = document.querySelector('.circunferenciaDelCirculo');
+const areaDelCirculo = document.querySelector('.areaDelCirculo');
+const alturaIsosceles = document.querySelector('.alturaTrianguloIsosceles');
+const alturaEscaleno = document.querySelector('.alturaTrianguloEscaleno');
 
 perimetroDelCuadrado.addEventListener('click', perimetroDelCuadradoF);
 areaDelCuadrado.addEventListener('click', areaDelCuadradoF);
 perimetroDelTriangulo.addEventListener('click', perimetroDelTrianguloF);
 areaDelTriangulo.addEventListener('click', areaDelTrianguloF);
+circunsferencia.addEventListener('click', circunsferenciaF);
+areaDelCirculo.addEventListener('click', areaDelCirculoF);
+alturaIsosceles.addEventListener('click', alturaIsoscelesF);
+alturaEscaleno.addEventListener('click', alturaEscalenoF);
 
+/* -------Cuadrado------- */
 
 function perimetroDelCuadradoF(e){
     e.preventDefault();
@@ -160,6 +169,8 @@ function calcularAreaDelCuadrado(){
         console.log('Ingresa un valor numérico');
     }
 } 
+
+/* -------Triangulo------- */
 
 function perimetroDelTrianguloF(e){
     e.preventDefault();
@@ -353,6 +364,372 @@ function calcularAreaDelTriangulo(){
     }
 
 
+}
+
+function alturaIsoscelesF(e){
+    e.preventDefault();
+
+    const pDescripcionPDC = document.createElement('p');
+    pDescripcionPDC.classList.add('descriptionPDC');
+    pDescripcionPDC.style.whiteSpace = 'pre-line';
+    pDescripcionPDC.innerText = 'Para calcular la altura de un triángulo isósceles:\nUsando el teorema de Pitágoras:\nh = √(a^2 - (b/2)^2)\nUsando trigonometría:\nh = (a/2) * tan(θ/2)\nDonde "a" es la longitud de los lados iguales, "b" es la base del triángulo y θ es el ángulo en la cima del triángulo.'
+
+    const formulaPDC = document.createElement('span');
+    formulaPDC.classList.add('formulaPDC');
+    formulaPDC.innerText = '(Base * Altura) / 2';
+
+    const figura = document.createElement('div')
+    figura.classList.add('triangulo');
+
+    const containerCode = document.createElement('div')
+    containerCode.classList.add('containerCode');
+    const code = document.createElement('span');
+    code.classList.add('code');
+    code.style.whiteSpace = 'pre-line';
+    code.textContent = 'const ladosEquivalentes = 10;\nconst base = 12;\nconst altura = Math.sqrt( (lado **2) - ( (base ** 2) ) / 4);';
+
+    const operacion = document.createElement('div');
+    operacion.classList.add('operacion');
+
+    const label1 = document.createElement('label');
+    const input1 = document.createElement('input');
+    input1.classList.add('input1');
+    label1.classList.add('label1');
+    label1.innerText = 'Base del triángulo';
+    input1.setAttribute('type', 'number');
+    input1.setAttribute('id', 'number');
+
+    const label2 = document.createElement('label');
+    const input2 = document.createElement('input');
+    input2.classList.add('input2');
+    label2.classList.add('label2');
+    label2.innerText = 'Lados del triángulo';
+    input2.setAttribute('type', 'number');
+    input2.setAttribute('id', 'number');
+
+    const nota = document.createElement('p');
+    nota.innerText = 'La base de un triangulo'
+
+
+    const button = document.createElement('button');
+    button.innerText = 'Calcular';
+    button.addEventListener('click', calcularAlturaIsosceles);
+
+    containerCode.appendChild(code)
+
+    label1.appendChild(input1);
+    label2.appendChild(input2);
+    operacion.appendChild(label1);
+    operacion.appendChild(label2);
+    operacion.appendChild(button)
+
+    containerInfo.appendChild(pDescripcionPDC);
+    containerInfo.appendChild(formulaPDC);
+    containerInfo.appendChild(figura);
+    containerInfo.appendChild(containerCode);
+    containerInfo.appendChild(operacion);
+
+    alturaIsosceles.removeEventListener('click', alturaIsoscelesF)
+}
+
+function calcularAlturaIsosceles(){
+    const inputValue1 = document.querySelector('.input1').value
+    const inputValue2 = document.querySelector('.input2').value
+    const base = parseFloat(inputValue1);
+    const altura = parseFloat(inputValue2);
+    const triangulo = document.querySelector('.triangulo');
+
+    const resultadoElement = triangulo.querySelector('.resultadoTriangulo');
+
+    if (!isNaN(base) && !isNaN(altura)) {
+        //const area = (base * altura) / 2;
+        function alturaIsosceles(base, lado) {
+            if(base === lado){
+                console.log('no es triangulo isosceles')
+            } else {
+                return Math.sqrt((lado ** 2) - ((base ** 2) / 4));
+            }
+        }
+
+        const resultadoAltura = alturaIsosceles(base, altura)
+
+        if (resultadoElement) {
+            resultadoElement.innerText = resultadoAltura;
+        } else {
+            const resultado = document.createElement('p');
+            resultado.classList.add('resultadoTriangulo');
+            resultado.innerText = resultadoAltura.toFixed(2);
+            triangulo.appendChild(resultado);
+        }
+
+        console.log( resultadoAltura);
+    } else {
+        console.log('Ingresa un valor numérico');
+    }
+}
+
+function alturaEscalenoF(e){
+    e.preventDefault();
+
+    const pDescripcionPDC = document.createElement('p');
+    pDescripcionPDC.classList.add('descriptionPDC');
+    pDescripcionPDC.style.whiteSpace = 'pre-line';
+    pDescripcionPDC.innerText = 'En el caso de que sólo tengamos los lados del triangulo vamos a calcular el semiperimetro\ns = (a + b + c)/2\nLuego sacamos el área\narea = RaizCuadradaDe(s*(s-base)*(s-lado1)*(s-lado))\nLuego calculamos la altura\naltura = (2 * area) / base';
+
+    const formulaPDC = document.createElement('span');
+    formulaPDC.classList.add('formulaPDC');
+    formulaPDC.innerText = 's = (a + b + c)/2\narea = RaizCuadradaDe(s*(s-base)*(s-lado1)*(s-lado))\naltura = (2 * area) / base';
+
+    const figura = document.createElement('div')
+    figura.classList.add('triangulo');
+
+    const containerCode = document.createElement('div')
+    containerCode.classList.add('containerCode');
+    const code = document.createElement('span');
+    code.classList.add('code');
+    code.style.whiteSpace = 'pre-line';
+    code.textContent = 'const s = (base + ladoB + ladoC) / 2;\nconst area = Math.sqrt(s * (s - base) * (s - ladoB) * (s - ladoC));\nconst altura = ((2 * area) / base);';
+
+    const operacion = document.createElement('div');
+    operacion.classList.add('operacion');
+
+    const labelA = document.createElement('label');
+    const inputA = document.createElement('input');
+    inputA.classList.add('inputA');
+    labelA.classList.add('labelA');
+    labelA.innerText = 'Lado 1 del triángulo (BASE)';
+    inputA.setAttribute('type', 'number');
+    inputA.setAttribute('id', 'number');
+
+    const labelB = document.createElement('label');
+    const inputB = document.createElement('input');
+    inputB.classList.add('inputB');
+    labelB.classList.add('labelB');
+    labelB.innerText = 'Lado 2 del triángulo';
+    inputB.setAttribute('type', 'number');
+    inputB.setAttribute('id', 'number');
+
+    const labelC = document.createElement('label');
+    const inputC = document.createElement('input');
+    inputC.classList.add('inputC');
+    labelC.classList.add('labelC');
+    labelC.innerText = 'Lado 3 del triángulo';
+    inputC.setAttribute('type', 'number');
+    inputC.setAttribute('id', 'number');
+
+    const button = document.createElement('button');
+    button.innerText = 'Calcular';
+    button.addEventListener('click', calcularAlturaEscaleno);
+
+    containerCode.appendChild(code)
+
+    labelA.appendChild(inputA);
+    labelB.appendChild(inputB);
+    labelC.appendChild(inputC);
+    operacion.appendChild(labelA);
+    operacion.appendChild(labelB);
+    operacion.appendChild(labelC);
+    operacion.appendChild(button)
+
+    containerInfo.appendChild(pDescripcionPDC);
+    containerInfo.appendChild(formulaPDC);
+    containerInfo.appendChild(figura);
+    containerInfo.appendChild(containerCode);
+    containerInfo.appendChild(operacion);
+
+    alturaIsosceles.removeEventListener('click', alturaEscalenoF)
+}
+
+function calcularAlturaEscaleno(){
+    const inputValue1 = document.querySelector('.inputA').value
+    const inputValue2 = document.querySelector('.inputB').value
+    const inputValue3 = document.querySelector('.inputC').value
+    const ladoA = parseFloat(inputValue1);
+    const ladoB = parseFloat(inputValue2);
+    const ladoC = parseFloat(inputValue3);
+    const triangulo = document.querySelector('.triangulo');
+
+    const resultadoElement = triangulo.querySelector('.resultadoTriangulo');
+
+    if (!isNaN(ladoA) && !isNaN(ladoB) && !isNaN(ladoC)) {
+
+        const s = (ladoA + ladoB + ladoC) / 2;
+
+        const area = Math.sqrt(s * (s - ladoA) * (s - ladoB) * (s - ladoC));
+
+        const altura = ((2 * area) / ladoA);
+        const resultadoAltura = area.toFixed(3)
+
+        console.log(resultadoAltura)
+
+        if (resultadoElement) {
+            resultadoElement.innerText = resultadoAltura;
+        } else {
+            const resultado = document.createElement('p');
+            resultado.classList.add('resultadoTriangulo');
+            resultado.innerText = resultadoAltura
+            triangulo.appendChild(resultado);
+        }
+
+    } else {
+        console.log('Ingresa un valor numérico');
+    }
+}
+
+/* -------Circulo------- */
+
+function circunsferenciaF(e){
+    e.preventDefault();
+
+    const pDescripcionPDC = document.createElement('p');
+    pDescripcionPDC.classList.add('descriptionPDC');
+    pDescripcionPDC.innerText = 'Para calcular la circunferencia de un círculo debemos multiplicar el diámetro del mismo por PI .'
+
+    const formulaPDC = document.createElement('span');
+    formulaPDC.classList.add('formulaPDC');
+    formulaPDC.innerText = 'Diámetro * PI';
+
+    const figura = document.createElement('div')
+    figura.classList.add('circulo');
+
+    const containerCode = document.createElement('div')
+    containerCode.classList.add('containerCode');
+    const code = document.createElement('span');
+    code.classList.add('code');
+    code.style.whiteSpace = 'pre-line';
+    code.textContent = 'const diametro = 20;\nconst circunferencia = diametro * Math.PI.toFixed(3);';
+
+    const operacion = document.createElement('div');
+    operacion.classList.add('operacion');
+
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    input.classList.add('input');
+    label.classList.add('label');
+    label.innerText = 'Diámetro del círculo';
+    input.setAttribute('type', 'number');
+    input.setAttribute('id', 'number');
+
+    const button = document.createElement('button');
+    button.innerText = 'Calcular';
+    button.addEventListener('click', calcularCircunferencia);
+
+    containerCode.appendChild(code)
+
+    label.appendChild(input);
+    operacion.appendChild(label);
+    operacion.appendChild(button)
+
+    containerInfo.appendChild(pDescripcionPDC);
+    containerInfo.appendChild(formulaPDC);
+    containerInfo.appendChild(figura);
+    containerInfo.appendChild(containerCode);
+    containerInfo.appendChild(operacion);
+
+    circunsferencia.removeEventListener('click', circunsferenciaF)
+}
+
+function calcularCircunferencia(){
+    const inputValue = document.querySelector('.input').value
+    const diametro = parseFloat(inputValue);
+    const circulo = document.querySelector('.circulo');
+
+    const resultadoElement = circulo.querySelector('.resultadoCirculo');
+
+    if (!isNaN(diametro)) {
+        const pi = Math.PI.toFixed(3)
+        const circunferencia = diametro * pi;
+
+        if (resultadoElement) {
+            resultadoElement.innerText = circunferencia.toFixed(3);
+        } else {
+            const resultado = document.createElement('p');
+            resultado.classList.add('resultadoCirculo');
+            resultado.innerText = circunferencia.toFixed(3);
+            circulo.appendChild(resultado);
+        }
+
+        console.log('El perímetro del cuadrado es: ' + circunferencia);
+    } else {
+        console.log('Ingresa un valor numérico');
+    }
+}
+
+function areaDelCirculoF(e){
+    e.preventDefault();
+
+    const pDescripcionPDC = document.createElement('p');
+    pDescripcionPDC.classList.add('descriptionPDC');
+    pDescripcionPDC.innerText = 'Calculamos el área de un círculo multiplicando el radio al cuadrado por PI.'
+
+    const formulaPDC = document.createElement('span');
+    formulaPDC.classList.add('formulaPDC');
+    formulaPDC.innerText = 'Radio² * PI';
+
+    const figura = document.createElement('div')
+    figura.classList.add('circulo');
+
+    const containerCode = document.createElement('div')
+    containerCode.classList.add('containerCode');
+    const code = document.createElement('span');
+    code.classList.add('code');
+    code.style.whiteSpace = 'pre-line';
+    code.textContent = 'const radio = 6;\nconst areaDelCirculo = Math.pow(radio, 2) * Math.PI;';
+
+    const operacion = document.createElement('div');
+    operacion.classList.add('operacion');
+
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    input.classList.add('input');
+    label.classList.add('label');
+    label.innerText = 'Radio del círculo';
+    input.setAttribute('type', 'number');
+    input.setAttribute('id', 'number');
+
+    const button = document.createElement('button');
+    button.innerText = 'Calcular';
+    button.addEventListener('click', calcularAreaDelCirculo);
+
+    containerCode.appendChild(code)
+
+    label.appendChild(input);
+    operacion.appendChild(label);
+    operacion.appendChild(button)
+
+    containerInfo.appendChild(pDescripcionPDC);
+    containerInfo.appendChild(formulaPDC);
+    containerInfo.appendChild(figura);
+    containerInfo.appendChild(containerCode);
+    containerInfo.appendChild(operacion);
+
+    circunsferencia.removeEventListener('click', areaDelCirculoF)
+}
+
+function calcularAreaDelCirculo(){
+    const inputValue = document.querySelector('.input').value
+    const radio = parseFloat(inputValue);
+    const circulo = document.querySelector('.circulo');
+
+    const resultadoElement = circulo.querySelector('.resultadoCirculo');
+
+    if (!isNaN(radio)) {
+        //const pi = Math.PI.toFixed(3)
+        const areaDelCirculo = Math.pow(radio, 2) * Math.PI.toFixed(1);
+
+        if (resultadoElement) {
+            resultadoElement.innerText = areaDelCirculo.toFixed(1);
+        } else {
+            const resultado = document.createElement('p');
+            resultado.classList.add('resultadoCirculo');
+            resultado.innerText = areaDelCirculo.toFixed(3);
+            circulo.appendChild(resultado);
+        }
+
+        console.log('El perímetro del cuadrado es: ' + areaDelCirculo);
+    } else {
+        console.log('Ingresa un valor numérico');
+    }
 }
 
 
